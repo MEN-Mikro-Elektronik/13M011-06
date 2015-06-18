be3fb494d35855d0844efbb9cdd2efbb9347d50d
thomas.schnuerer <thomas.schnuerer@men>
2015-06-18 10:38:44 +0200

    R: added helptext example

--- TOOLS/M_TMR/M_TMR_VERI/COM/m_tmr_veri.c
+++ TOOLS/M_TMR/M_TMR_VERI/COM/m_tmr_veri.c
@@ -84,10 +84,11 @@ static void usage(void)
 {
 	printf("Usage: m_tmr_veri [<opts>] <device> [<opts>]\n\n");
 	printf("Function: Verification tool for MDIS drivers implementing TMR profile\n");
-	printf("  device       MDIS device name                [none]    \n");
+	printf("  device       device name                     [none]    \n");
 	printf("Options:\n");
 	printf("  -c=<dec>     channel number                  [1]       \n");
 	printf("\n");
+	printf("Example: m_tmr_veri m11_1 -c=2                 verify channel 2\n");
 	printf("(c) 1999-2015 MEN mikro elektronik GmbH\n\n");
 }
 
