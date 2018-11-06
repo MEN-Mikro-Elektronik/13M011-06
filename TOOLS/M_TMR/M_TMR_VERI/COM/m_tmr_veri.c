e25c5097dbc9c7bfe5cd40b6489afbeafab153d0
KonradJPE <ktomasik@jpembedded.eu>
2018-11-06 14:19:52 +0100

    Remove history, add GPL license to file

--- TOOLS/M_TMR/M_TMR_VERI/COM/m_tmr_veri.c
+++ TOOLS/M_TMR/M_TMR_VERI/COM/m_tmr_veri.c
@@ -11,21 +11,21 @@
  *     Required: libraries: mdis_api, usr_oss, usr_utl
  *     Switches: -
  *
- *-------------------------------[ History ]---------------------------------
- *
- * $Log: m_tmr_veri.c,v $
- * Revision 1.3  2013/09/10 11:01:24  gv
- * R: Porting to MDIS5
- * M: Changed according to MDIS porting guide 0.9
- *
- * Revision 1.2  2001/02/08 15:45:27  kp
- * Problem fix: declare G_sigCnt as volatile
- * (problem occurred with GCC/VxWorks)
- *
- * Revision 1.1  1999/11/03 15:40:11  kp
- * Initial Revision
- *
  ****************************************************************************/
+ /*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <string.h>
